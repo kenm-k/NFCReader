6aa1356dd3bc5e9efa8d02f86a64ddcd0103e840
kenm-k <minaken0584@gmail.com>
2025-04-02 19:22:42 +0900

    ちょっと危ないかも

---
+++ apikey.js
@@ -0,0 +1,6 @@
+const API_KEY='AIzaSyAmSWQ_EHaV_xJxVGBdz6w3Bzg_7RN-sIw'
+const AUTH_DOMAIN='iddb-f7c51.firebaseapp.com'
+const PROJECT_ID='iddb-f7c51'
+const STORAGE_BUCKET='iddb-f7c51.firebasestorage.app'
+const MESSAGING_SENDER_ID='989836694404'
+const APP_ID='1:989836694404:web:078cb2cda7f21cab6643d1'
